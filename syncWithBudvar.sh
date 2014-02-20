4dc8eaae789926c329ef403518cdaea2fe2819cf
Jan Kučera <kucera.ja@gmail.com>
2014-02-20 09:51:19 -0600

    better sync excluding .git folder

--- syncWithBudvar.sh
+++ syncWithBudvar.sh
@@ -1,1 +1,1 @@
-rsync -r -p -l -u --progress --stats -t --exclude=GLM /home/kujan/Documents/FJFI/FNAL-INGO/root_scripts/ /media/sclued0/work/budvar-clued0/kuceraja/FNAL/scripts/ 
+rsync -r -p -l -u --progress --stats -t --exclude=GLM --exclude=.git /home/kujan/Documents/FJFI/FNAL-INGO/root_scripts/ /media/sclued0/work/budvar-clued0/kuceraja/FNAL/scripts/
