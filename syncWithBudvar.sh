acaea86796ce1fb5e8b25522997b6538dcd22b3f
Jan Kučera <kucera.ja@gmail.com>
2014-02-20 10:00:02 -0600

    fixed sync. Won't bitch about failed setting timestamp on the links input_lepton_1119

--- syncWithBudvar.sh
+++ syncWithBudvar.sh
@@ -1,1 +1,1 @@
-rsync -r -p -l -u --progress --stats -t --exclude=GLM --exclude=.git /home/kujan/Documents/FJFI/FNAL-INGO/root_scripts/ /media/sclued0/work/budvar-clued0/kuceraja/FNAL/scripts/
+rsync -r -p -l -u --progress --stats -t --exclude=GLM --exclude=inputs_ele_1119 --exclude=inputs_muo_1119 --exclude=.git /home/kujan/Documents/FJFI/FNAL-INGO/root_scripts/ /media/sclued0/work/budvar-clued0/kuceraja/FNAL/scripts/
